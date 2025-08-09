ecb8bc9949f587752459e81f402a9d16a12fcd93
hachem-dhawadi <hachemdhawadi2001@gmail.com>
2025-08-09 19:12:40 +0100

    ros2 control done (error fixed)

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,93 +1,87 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
-import xacro
-import yaml
+
+
 
 def generate_launch_description():
-    package_name = 'synhive_robot'
-    pkg_path = get_package_share_directory(package_name)
-    
-    # Process URDF
-    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_description = robot_description_config.toxml()
 
-    # Pass robot_description to rsp.launch.py
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='synhive_robot' #<--- CHANGE ME
+
+    urdf_file = os.path.join(
+        get_package_share_directory(package_name),
+        'description',
+        'robot.urdf.xacro'
+    )
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(pkg_path, 'launch', 'rsp.launch.py')
-        ]),
-        launch_arguments={
-            'use_sim_time': 'true',
-            'robot_description': robot_description
-        }.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Gazebo launch
+    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-        ])
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
     )
 
-    # Robot spawner with delay
-    spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    controllers_yaml = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'my_controllers.yaml'
+    )
+
+    # Controller manager node
+    controller_manager_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[
+            {'robot_description': robot_desc},
+            controllers_yaml
+        ],
         output='screen'
     )
-    delayed_spawn = TimerAction(period=6.0, actions=[spawn_entity])
 
-    # Controller manager with proper parameters
-    controllers_yaml = os.path.join(pkg_path, 'config', 'my_controllers.yaml')
-    with open(controllers_yaml, 'r') as f:
-        controller_params = yaml.safe_load(f)
-    
-    controller_manager_params = {
-        'controller_manager': controller_params['controller_manager']
-    }
-
-
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            {'robot_description': robot_description},
-            controller_manager_params  # PASS EXTRACTED PARAMS
-        ],
-        output="screen",
-    )
-
-    # Controller spawners with DELAY
-    load_joint_state = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
-        output="screen",
-    )
-
-    load_diff_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_controller", "-c", "/controller_manager"],
-        output="screen",
-    )
-
-    # ADD CONTROLLER DELAY
-    delayed_controllers = TimerAction(
-        period=8.0,
-        actions=[load_joint_state, load_diff_controller]
-    )
-
+    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
-        controller_manager,
-        delayed_spawn,
-        delayed_controllers,  # USE DELAYED CONTROLLERS
+        spawn_entity,
+        controller_manager_node,  # <-- Add this line
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
